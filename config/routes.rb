082960a84c20ccc50f54cda8b70caedb69fb19e4
Adam Podraza <apodra86@gmail.com>
2017-03-22 19:18:39 -0500

    Login with Facebook added

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'auth/:provider/callback', to: 'sessions#create'
+  match 'sign_out', to: 'sessions#destroy', via: :delete
+
 end
